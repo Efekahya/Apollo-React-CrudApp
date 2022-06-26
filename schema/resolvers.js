98404cfd22645db5bd7218862030077d9cd3f721
Efe Kahyaoğlu <75262924+Efekahya@users.noreply.github.com>
2022-06-26 16:57:38 +0300

    json link updated

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -3,7 +3,7 @@ const { argsToArgsConfig } = require("graphql/type/definition");
 
 let characters = axios
   .get(
-    "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/12e5c19f-cd6c-4069-a797-9720f280c57d/ricky-and-morty.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220623%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220623T222009Z&X-Amz-Expires=86400&X-Amz-Signature=64929cc88032e235d98f20b8456d7529c2ac4c2b878ccfe2ea1bbb80a09a12f8&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22ricky-and-morty.json%22&x-id=GetObject"
+    "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/12e5c19f-cd6c-4069-a797-9720f280c57d/ricky-and-morty.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220626%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220626T135633Z&X-Amz-Expires=86400&X-Amz-Signature=9390c5c214ade8084b2136a327a1777cd9e59ebb0de3a2272bd4e9f58e567a1a&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22ricky-and-morty.json%22&x-id=GetObject"
   )
   .then((res) => {
     characters = res.data
